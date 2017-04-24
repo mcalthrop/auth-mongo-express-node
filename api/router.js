1df5232e4a20e0b8a7d73168c9237ff8929e4c04
Matt Calthrop <mcalthrop@gmail.com>
2017-04-24 09:11:40 +0100

    Rename directories for auth and me controllers

--- api/router.js
+++ api/router.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const passport = require('passport');
 const permission = require('permission');
-const AuthController = require('./controllers/auth');
-const MeController = require('./controllers/me');
+const AuthController = require('./controllers/auth.controller');
+const MeController = require('./controllers/me.controller');
 const MetaSerializer = require('./serializers/meta.serializer');
 
 router.get('/', (req, res) => {
